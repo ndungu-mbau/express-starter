a3da6d69bed4e1ccf9fe11c2798f2c1646dac6e4
Nelson Mbau <mbau.ndungu@gmail.com>
2026-04-20 12:43:28 +0300

    feat: mount routers, static
    - Mount the views from the view models, both api and rendered views
    - Add static file resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express"
 import { config } from "dotenv"
+import { apiRouter } from './views/api'
+import { rendererRouter } from './views/renderer'
+import path from "path"
 
 config()
 
@@ -10,9 +13,17 @@ const port = Number(process.env.PORT) || 3000
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, 'templates'))
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
 app.get('/health', (req, res) => {
     res.json({ message: "Application is up and healthy" })
 })
+
+app.use('/views', rendererRouter)
+app.use('/api', apiRouter)
 
 app.listen(port, () => console.log(`Application running at port ${port}`))
 
