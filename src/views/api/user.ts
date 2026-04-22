d5f93e05361846c5c725c4d7c993dc103a96bc28
Nelson Mbau <mbau.ndungu@gmail.com>
2026-04-22 09:12:17 +0300

    feat: call new version of userController

--- src/views/api/user.ts
+++ src/views/api/user.ts
@@ -1,22 +1,31 @@
 import { Router } from "express";
-import expressAsyncHandler from "express-async-handler";
-import { readAllUsers, getUserById } from "../../controllers/user";
+import { userController } from "../../controllers";
 
 const usersRouter = Router()
 
-usersRouter.get('/', expressAsyncHandler(async (req, res) => {
-    const users = await readAllUsers()
 
-    res.json({ users })
-}))
+usersRouter.get('/', async (req, res) => {
+    const { email } = req.query
+    if (email && typeof email === 'string') {
+        const user = await userController.findByEmail(email)
+        res.json({ data: [user] })
+        return
+    }
+    const data = await userController.findAll()
+    res.json({ data })
+})
 
-usersRouter.get('/:id', expressAsyncHandler(async (req, res) => {
+usersRouter.get('/:id', async (req, res) => {
     const id: string = req.params.id as string
-
-    const user = await getUserById(id)
-
+    const user = await userController.findById(id)
     res.json({ user })
-}))
+})
+
+usersRouter.post('/', async (req, res) => {
+    const newUser = req.body
+    const createdUser = await userController.create(newUser)
+    res.json({ data: createdUser })
+})
 
 export {
     usersRouter
--- src/views/renderer/user.ts
+++ src/views/renderer/user.ts
@@ -1,23 +1,33 @@
 import { Router } from 'express'
-import expressAsyncHandler from 'express-async-handler'
 
-import { readAllUsers, getUserById } from '../../controllers/user'
+import { userController } from '../../controllers'
 
 const usersRouter = Router()
 
-usersRouter.get('/', expressAsyncHandler(async (req, res) => {
-    const users = await readAllUsers()
+usersRouter.get('/', async (req, res) => {
+    const { email } = req.query
+
+    if (email) {
+        const user = await userController.findByEmail(email as string)
+        res.render('user', {
+            user,
+            title: `User with email ${email}`
+        })
+        return
+    }
+
+    const users = await userController.findAll()
 
     res.render('users', {
         users,
         title: 'All Users'
     })
-}))
+})
 
-usersRouter.get('/:id', expressAsyncHandler(async (req, res) => {
+usersRouter.get('/:id', async (req, res) => {
     const userId = req.params.id as string
 
-    const user = await getUserById(userId)
+    const user = await userController.findById(userId)
 
     if (!user) {
         return res.status(404).render('404', { title: "User not found", message: "User not found" })
@@ -27,7 +37,7 @@ usersRouter.get('/:id', expressAsyncHandler(async (req, res) => {
         user,
         title: `User ${user.name}`
     })
-}))
+})
 
 export {
     usersRouter
